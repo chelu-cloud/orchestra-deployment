80f64eb05cd90cb12d5d59bf4a158c8610f00205
José Luis Salvador <chelusthetic@gmail.com>
2026-05-05 19:05:18 +0200

    add: deploy.yaml

--- despliegue/terraform/modules/iam/main.tf
+++ despliegue/terraform/modules/iam/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_policy" "policy" {
 }
 
 resource "aws_iam_role" "kubernetes-oidc-role" {
-  name = "eks-venganza"
+  name = "kubernetes-oidc-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -28,7 +28,7 @@ resource "aws_iam_role" "kubernetes-oidc-role" {
         }
         Condition = {
           StringEquals = {
-            "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:orchestra-namespace:backend-sa",
+            "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:orchestra-namespace:service-account-orchestra-backend",
             "${replace(var.oidc_provider_url, "https://", "")}:aud" = "sts.amazonaws.com"
           }
         } 
@@ -41,8 +41,6 @@ resource "aws_iam_role_policy_attachment" "backend_attach" {
   role       = aws_iam_role.kubernetes-oidc-role.name
   policy_arn = aws_iam_policy.policy.arn
 }
-
-# ---------
 
 resource "aws_iam_role" "eks-cluster-role" {
   name = "eks-cluster-role"
@@ -65,7 +63,6 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks-cluster-role.name
 }
 
-
 resource "aws_iam_role" "eks-worker-node-role" {
   name = "eks-worker-node-role"
   assume_role_policy = jsonencode({
@@ -86,11 +83,87 @@ resource "aws_iam_role_policy_attachment" "eks-worker-noder_policy_CNI" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.eks-worker-node-role.name
 }
+
 resource "aws_iam_role_policy_attachment" "eks-worker-noder_policy_Node" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks-worker-node-role.name
 }
+
 resource "aws_iam_role_policy_attachment" "eks-worker-noder_policy_EC2_CRegistry" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks-worker-node-role.name
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:chelu-cloud/orchestra-deployment:*"
+          }
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_eks_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterAdminPolicy" 
+}
+
+data "http" "alb_controller_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller_policy" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "Policy for AWS Load Balancer Controller"
+  policy      = data.http.alb_controller_policy.response_body
+}
+
+resource "aws_iam_role" "aws_load_balancer_controller_role" {
+  name = "aws-load-balancer-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = var.oidc_provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "${replace(var.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller",
+            "${replace(var.oidc_provider_url, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.aws_load_balancer_controller_role.name
+  policy_arn = aws_iam_policy.alb_controller_policy.arn
 }
